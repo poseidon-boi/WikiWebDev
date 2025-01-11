b48dab10660b945d0fd21ce900cf75ecc86ba4ed
Poseidon <armaan_0517@yahoo.com>
2025-01-11 16:02:18 +0530

    Table of contents now highlights the current section

--- MBMTask1/script.js
+++ MBMTask1/script.js
@@ -31,35 +31,110 @@ const initApp = () => {
 		});
 	}
 
-	const anchor = document.querySelector("footer a");
-	console.log(anchor);
+	// const anchor = document.querySelector("footer a");
+	// console.log(anchor);
 
-	let scamState = 0;
+	// let scamState = 0;
 
-	anchor.addEventListener("mouseover", (event) => {
-		if (scamState === 0) {
-			console.log("Scammed");
-			footer.style.textAlign = "right";
-			event.target.textContent = "Can't touch me";
-			scamState = 1;
-		} else if (scamState === 1) {
-			footer.style.textAlign = "center";
-			event.target.textContent = "Nope";
-			scamState = 2;
-		} else if (scamState === 2) {
-			footer.style.textAlign = "right";
-			footer.style.backgroundColor = "lightblue";
-			event.target.textContent = "Ok u win";
-			scamState = 3;
-		}
+	// anchor.addEventListener("mouseover", (event) => {
+	// 	if (scamState === 0) {
+	// 		console.log("Scammed");
+	// 		footer.style.textAlign = "right";
+	// 		event.target.textContent = "Can't touch me";
+	// 		scamState = 1;
+	// 	} else if (scamState === 1) {
+	// 		footer.style.textAlign = "center";
+	// 		event.target.textContent = "Nope";
+	// 		scamState = 2;
+	// 	} else if (scamState === 2) {
+	// 		footer.style.textAlign = "right";
+	// 		footer.style.backgroundColor = "lightblue";
+	// 		event.target.textContent = "Ok u win";
+	// 		scamState = 3;
+	// 	}
+	// });
+	// anchor.addEventListener("click", (event) => {
+	// 	console.log("Back to top");
+	// 	footer.style.textAlign = "left";
+	// 	footer.style.backgroundColor = null;
+	// 	event.target.textContent = "Back to top";
+	// 	scamState = 0;
+	// });
+
+	const articles = document.querySelectorAll("article");
+	const titles = document.querySelectorAll("#tableOfContents a");
+	const observers = new Array();
+
+	window.addEventListener("scroll", (event) => {
+		articles.forEach((article) => {
+			const heading = article.querySelector("h2");
+			const rect = article.getBoundingClientRect();
+			if (
+				(rect.top >= 0 && rect.top < window.innerHeight / 2) ||
+				(rect.bottom <= window.innerHeight &&
+					rect.bottom >= window.innerHeight / 2)
+			) {
+				titles.forEach((title) => {
+					if (
+						heading.textContent.toLowerCase() ===
+						title.textContent.toLowerCase()
+					) {
+						title.style.borderLeftStyle = "solid";
+						title.style.borderLeftColor = "green";
+						title.style.borderLeftWidth = "5px";
+						title.style.fontSize = "1.5rem";
+					} else {
+						title.style.borderLeftWidth = "0";
+						title.style.fontSize = "1.3rem";
+					}
+				});
+			}
+		});
 	});
-	anchor.addEventListener("click", (event) => {
-		console.log("Back to top");
-		footer.style.textAlign = "left";
-		footer.style.backgroundColor = null;
-		event.target.textContent = "Back to top";
-		scamState = 0;
-	});
+
+	// for (let i = 0; i < articles.length; i++) {
+	// 	const article = articles[i];
+	// const heading = article.querySelector("h2");
+	// console.log(heading.textContent);
+	// console.log(titles[i].textContent);
+	// console.log(heading.textContent === titles[i].textContent);
+	// article.addEventListener("", (event) => {
+	// 	console.log("Scrolled");
+	// for (let j = 0; j < titles.length; j++) {
+	// 	const title = titles[j];
+	// 	console.log(title);
+	// 	if (heading.textContent === title.textContent) {
+	// 		title.style.classList.toggle("scrolledTo");
+	// 	}
+	// 	}
+	// });
+	// const observer = new IntersectionObserver((entries, observer) => {
+	// 	entries.forEach((entry) => {
+	// 		if (entry.intersectionRatio > 0) {
+	// 			console.log("Triggered");
+	// 			console.log(observer);
+	// 			console.log(entry);
+	// 			const heading = entry.target.querySelector("h2");
+	// 			for (let j = titles.length - 1; j >= 0; j--) {
+	// 				const title = titles[j];
+	// 				console.log(title);
+	// if (heading.textContent === title.textContent) {
+	// 	title.style.borderLeftStyle = "solid";
+	// 	title.style.borderLeftColor = "green";
+	// 	title.style.borderLeftWidth = "5px";
+	// 	title.style.fontSize = "1.5rem";
+	// } else {
+	// 	title.style.borderLeftWidth = "0";
+	// 	title.style.fontSize = "1.3rem";
+	// 				}
+	// 			}
+	// 		}
+	// 	});
+	// });
+	// observer.observe(article);
+	// observers.push(observer);
+	// }
+
 	// anchor.addEventListener("mouseout", (event) => {
 	//     footer.style.textAlign = "left";
 	//     event.target.textContent = "Back to top";
